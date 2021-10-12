7e56759004f90152ddac7da93ebab9b4da4ae483
LucyRandall99 <lucy.randall123@gmail.com>
2021-10-12 12:44:38 +0100

    prosperity-4 Refactor Code to Add Functions

--- soil-moisture-sensor/app.py
+++ soil-moisture-sensor/app.py
@@ -1,26 +1,28 @@
-from counterfit_connection import CounterFitConnection
-CounterFitConnection.init('127.0.0.1', 5000)
-
 import time
-from counterfit_shims_grove.adc import ADC
-from counterfit_shims_grove.grove_relay import GroveRelay
 import json
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
+from counterfit_shims_grove.adc import ADC
+from counterfit_shims_grove.grove_relay import GroveRelay
+from counterfit_connection import CounterFitConnection
 
 connection_string = 'HostName=anyaiothub.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=QRoksSHbOVbgt0BHz+xJdyzrMqsq1ht8n/Y8pif8mvs='
 
 adc = ADC()
 relay = GroveRelay(5)
 
-device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+def iotHubConnection(): 
+    CounterFitConnection.init('127.0.0.1', 5000)
+    device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
 
-print('Connecting')
-device_client.connect()
-print('Connected')
+    print('Connecting')
+    device_client.connect()
+    print('Connected')
+    return device_client
+
 
 def handle_method_request(request):
     print("Direct method received - ", request.name)
-    
+
     if request.name == "relay_on":
         relay.on()
     elif request.name == "relay_off":
@@ -29,13 +31,18 @@ def handle_method_request(request):
     method_response = MethodResponse.create_from_method_request(request, 200)
     device_client.send_method_response(method_response)
 
-device_client.on_method_request_received = handle_method_request
+def readADC():
+        soil_moisture = adc.read(0)
+        print("Soil moisture:", soil_moisture)
 
-while True:
-    soil_moisture = adc.read(0)
-    print("Soil moisture:", soil_moisture)
+        message = Message(json.dumps({'soil_moisture': soil_moisture}))
+        device_client.send_message(message)
 
-    message = Message(json.dumps({ 'soil_moisture': soil_moisture }))
-    device_client.send_message(message)
+def main(): 
+    device_client.on_method_request_received = handle_method_request
+    while True:
+        readADC()
+        time.sleep(10)
 
-    time.sleep(10)
+device_client = iotHubConnection()
+main()
