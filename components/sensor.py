81c637bb517e6fb66e28e52162beccba7e84b681
AS <anya.sewell1@btinternet.com>
2021-10-14 15:14:20 +0100

    Prosperity-7 : Creating test for sensor.py

--- components/sensor.py
+++ components/sensor.py
@@ -5,16 +5,18 @@ from counterfit_shims_grove.adc import ADC
 from counterfit_shims_grove.grove_relay import GroveRelay
 from counterfit_connection import CounterFitConnection
 
-connection_string = 'HostName=anyaiothub.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=QRoksSHbOVbgt0BHz+xJdyzrMqsq1ht8n/Y8pif8mvs='
 
 adc = ADC()
 relay = GroveRelay(5)
 
-def iotHubConnection(): 
+def counterfit_connection():
     CounterFitConnection.init('127.0.0.1', 5000)
+
+def iot_hub_connection(connection_string): 
     device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
 
     print('Connecting')
+    print(device_client + "test version")
     device_client.connect()
     print('Connected')
     return device_client
@@ -31,7 +33,7 @@ def handle_method_request(request):
     method_response = MethodResponse.create_from_method_request(request, 200)
     device_client.send_method_response(method_response)
 
-def readADC():
+def read_adc():
         soil_moisture = adc.read(0)
         print("Soil moisture:", soil_moisture)
 
@@ -41,9 +43,11 @@ def readADC():
 def main(): 
     device_client.on_method_request_received = handle_method_request
     while True:
-        readADC()
+        read_adc()
         time.sleep(10)
 
 if __name__ == "__main__":
-    device_client = iotHubConnection()
+    counterfit_connection()
+    connection_string = 'HostName=anyaiothub.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=QRoksSHbOVbgt0BHz+xJdyzrMqsq1ht8n/Y8pif8mvs='
+    device_client = iot_hub_connection(connection_string)
     main()
